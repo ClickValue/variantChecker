7bd1726c12d0599103634fa866cd24766c79bcae
KennMensah <acaibravo@hotmail.com>
2022-12-06 14:02:51 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,9 +1,4 @@
 var script = document.createElement('script')
-// let qubit = [];
-// let optimize = [];
-// let optimizely = [];
-// let abtasty = [];
-
 script.src = chrome.runtime.getURL('script.js')
 document.head.appendChild(script);
 console.log('injecting content script');
