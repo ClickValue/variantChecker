a9c5ebc44d23248efdf005fe8a933409184858f8
KennMensah <acaibravo@hotmail.com>
2022-12-06 13:15:37 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -6,4 +6,4 @@ var script = document.createElement('script')
 
 script.src = chrome.runtime.getURL('script.js')
 document.head.appendChild(script);
-console.log('injected content script');
+console.log('injecting content script');
